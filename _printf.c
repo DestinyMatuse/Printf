3c7ffd28fa498a1f9c5ddef5cb98791998f17665
isaacprince <isaacprince002@gmail.com>
2023-07-26 17:12:10 -0700

    chore:main.h,_printf

--- _printf.c
+++ _printf.c
@@ -67,9 +67,9 @@ int _printf(const char *format, ...)
 			print_buffer(buffer, &buff_ind);
 			size = get_size(format, &i);
 			flags = get_flags(format, &i);
-			width = print_width(format, &i, list);
-			precision = print_precision(format, &i, list);
-			size = printSize(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
 			++i;
 
 			printed = handle_print(format, &i, list, buffer,
